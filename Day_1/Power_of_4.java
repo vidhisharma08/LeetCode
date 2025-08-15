9433748512d9b4c2cd524c4fa4d3f2855ed11d52
vidhisharma08 <vidhisharma2908@gmail.com>
2025-08-15 15:59:11 +0530

    Day 1 - Added Power of 4 problem

---
+++ Day_1/Power_of_4.java
@@ -0,0 +1,12 @@
+package Day_1;
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        long i=1;
+         while(i<n){
+            i*=4;
+         }
+         if(n==i) return true;
+         else return false;
+    }
+}
