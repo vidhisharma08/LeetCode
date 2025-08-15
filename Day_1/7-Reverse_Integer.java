fb11ceed3a5b66f547f4ea83cb51c3aff6fd9567
vidhisharma08 <vidhisharma2908@gmail.com>
2025-08-15 16:39:29 +0530

    Day 2 - Reverse Integer problem

---
+++ Day_1/7-Reverse_Integer.java
@@ -0,0 +1,21 @@
+package Day_1;
+
+class Solution {
+    public int reverse(int x) {
+        int m=x;
+        int l=0;
+        if(x<0){
+            x=(-2*x)+m;
+        }
+        while(x>0){
+            int r=x%10;
+            if(l<Integer.MIN_VALUE/10 || l>Integer.MAX_VALUE/10){
+                return 0;
+            }
+            l=(l*10)+r;
+            x=x/10;
+        }
+        if(m<0) return (-1)*l;
+        else return l;
+    }
+}
