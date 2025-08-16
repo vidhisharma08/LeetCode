dfe53583d44095b7579ec685d9b3bd715d1f891f
vidhisharma08 <vidhisharma2908@gmail.com>
2025-08-16 22:36:41 +0530

    max 69 number solution

---
+++ Day_1/Max_69_Number.java
@@ -0,0 +1,14 @@
+package Day_1;
+
+class Solution {
+    public int maximum69Number (int num) {
+        StringBuilder s=new StringBuilder(String.valueOf(num));
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='6'){
+                s.setCharAt(i,'9');
+                break;
+            }
+        }
+        return Integer.valueOf(s.toString());
+    }
+}
